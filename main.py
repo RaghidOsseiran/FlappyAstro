27cd806b1912e3b87d743dd259ad94a125ee3ffe
RaghidOsseiran <147770887+RaghidOsseiran@users.noreply.github.com>
2023-11-16 11:26:58 +0100

    Added collisions between rectangles and also with mouse

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ clock = pg.time.Clock()
 test_font = pg.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pg.image.load("graphics/Sky.png").convert()
-ground_surface = pg.image.load("graphics/Ground.png").convert()
+ground_surface = pg.image.load("graphics/ground.png").convert()
+
 text_surface = test_font.render('My game', False, 'black')
+text_rect = text_surface.get_rect(center = (400,50))
 
 snail_surface = pg.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (600,300))
@@ -23,17 +25,27 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             exit()
+        # if event.type == pg.MOUSEMOTION:
+        #     if (player_rect.collidepoint(event.pos)): print("collision")
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (325,0))
+    screen.blit(text_surface, text_rect)
 
     snail_rect.left -= 2
     if snail_rect.right == 0 : snail_rect.left = 800
 
     screen.blit(snail_surface, snail_rect)
-
-    player_rect.left += 1
     screen.blit(player_surf, player_rect)
+
+    # check for collisions between my player rectangle and the snail rectangle
+
+    # if (player_rect.colliderect(snail_rect)):
+    #     print("collision")
+
+    # mouse_pos = pg.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print(pg.mouse.get_pressed())
+
     pg.display.update()
     clock.tick(60)
